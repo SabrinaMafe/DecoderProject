95c1db7e699b3b2625aefb1a0669b1041e323775
Sabrina Mafé <58732017+SabrinaMafe@users.noreply.github.com>
2022-08-16 07:10:23 -0300

    create json viewer

--- authuser/src/main/java/com/ead/authuser/dtos/UserDto.java
+++ authuser/src/main/java/com/ead/authuser/dtos/UserDto.java
@@ -5,6 +5,7 @@ import com.ead.authuser.enums.UserType;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -16,21 +17,37 @@ import java.util.UUID;
 public class UserDto {
 
 
+    public interface UserView{
+        public static interface RegistrationPost{}
+        public static interface UserPut{}
+        public static interface PasswordPut{}
+        public static interface ImagePut{}
+    }
+
     private UUID userId;
 
+    @JsonView(UserView.RegistrationPost.class)
     private String username;
 
+    @JsonView(UserView.RegistrationPost.class)
     private String email;
 
+    @JsonView({UserView.RegistrationPost.class, UserView.PasswordPut.class})
     private String password;
 
+    @JsonView(UserView.PasswordPut.class)
     private String OldPassword;
 
+    @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String fullName;
 
+    @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String phoneNumber;
+
+    @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String cpf;
 
+    @JsonView(UserView.ImagePut.class)
     private String imageUrl;
 
 
