b903eb68d9b1212d5141dbdb1dae6d6e65853b64
Sabrina Mafé <58732017+SabrinaMafe@users.noreply.github.com>
2022-08-24 06:42:59 -0300

    creating restTemplate for communication com other microsservice course

---
+++ authuser/src/main/java/com/ead/authuser/dtos/CourseDto.java
@@ -0,0 +1,72 @@
+package com.ead.authuser.dtos;
+
+import com.ead.authuser.enums.CourseLevel;
+import com.ead.authuser.enums.CourseStatus;
+
+import java.util.UUID;
+
+public class CourseDto {
+    private UUID courseId;
+    private String name;
+    private String description;
+    private String imageUrl;
+    private CourseStatus courseStatus;
+    private UUID userInstructor;
+    private CourseLevel courseLevel;
+
+    public UUID getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(UUID courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public CourseStatus getCourseStatus() {
+        return courseStatus;
+    }
+
+    public void setCourseStatus(CourseStatus courseStatus) {
+        this.courseStatus = courseStatus;
+    }
+
+    public UUID getUserInstructor() {
+        return userInstructor;
+    }
+
+    public void setUserInstructor(UUID userInstructor) {
+        this.userInstructor = userInstructor;
+    }
+
+    public CourseLevel getCourseLevel() {
+        return courseLevel;
+    }
+
+    public void setCourseLevel(CourseLevel courseLevel) {
+        this.courseLevel = courseLevel;
+    }
+}
