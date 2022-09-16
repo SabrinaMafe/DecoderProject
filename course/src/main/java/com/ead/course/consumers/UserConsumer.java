d7f755fe8b27464819ecab3c3e9dab08f30d00d9
Sabrina Mafé <58732017+SabrinaMafe@users.noreply.github.com>
2022-09-16 07:14:49 -0300

    adding username and password in all microservices by connect in microservice config server and service registry

--- course/src/main/java/com/ead/course/consumers/UserConsumer.java
+++ course/src/main/java/com/ead/course/consumers/UserConsumer.java
@@ -19,7 +19,7 @@ public class UserConsumer {
     UserService userService;
 
     @RabbitListener(bindings = @QueueBinding(value = @Queue(value = "${ead.broker.queue.userEventQueue.name}", durable = "true"),
-    exchange = @Exchange(value = "${ead.broker.exchange.userEvent}", type = ExchangeTypes.FANOUT, ignoreDeclarationExceptions = "true")))
+    exchange = @Exchange(value = "${ead.broker.exchange.userEventExchange}", type = ExchangeTypes.FANOUT, ignoreDeclarationExceptions = "true")))
     public void listenUserEvent(@Payload UserEventDto userEventDto){
         var userModel = userEventDto.convertToUserModel();
 
