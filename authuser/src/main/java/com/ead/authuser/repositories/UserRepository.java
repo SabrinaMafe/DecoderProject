f9a14b60b9de426d32de1551ab81bf9a9fc19067
Sabrina Mafé <58732017+SabrinaMafe@users.noreply.github.com>
2022-09-20 06:20:25 -0300

    configuring websecurity, and implements userDetails for save in database, adding roles for type user

--- authuser/src/main/java/com/ead/authuser/repositories/UserRepository.java
+++ authuser/src/main/java/com/ead/authuser/repositories/UserRepository.java
@@ -4,9 +4,11 @@ import com.ead.authuser.models.UserModel;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<UserModel, UUID>, JpaSpecificationExecutor<UserModel> {
@@ -14,5 +16,8 @@ public interface UserRepository extends JpaRepository<UserModel, UUID>, JpaSpeci
     boolean existsByUsername (String username);
 
     boolean existsByEmail(String email);
+
+    @EntityGraph(attributePaths = "roles", type = EntityGraph.EntityGraphType.FETCH)
+    Optional<UserModel> findByUsername(String username);
 
 }
